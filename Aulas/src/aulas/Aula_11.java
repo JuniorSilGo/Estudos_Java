dc4ccfba4a52b746897595adf1a16e4478afc6da
Junior <junior.silgo@gmail.com>
2021-07-08 17:56:06 -0300

    adicionado Aula 11

---
+++ Aulas/src/aulas/Aula_11.java
@@ -0,0 +1,21 @@
+package aulas;
+
+import java.util.Scanner;
+
+public class Aula_11{
+    
+    public static void main(String[] args){
+        
+        double base, altura, areaRetangulo;
+        
+        Scanner entrada = new Scanner(System.in);
+        areaRetangulo = base*altura;
+        
+        System.out.println("Entre com o valor da base: ");
+        
+        System.out.println("Entre com o valor da altura: ");
+        
+        System.out.println("A área do retângulo vale: "+areaRetangulo)
+                
+    }
+}
